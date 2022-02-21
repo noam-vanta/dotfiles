a173c27fb368bf7b50ac0066725af495c8b657d6
Noam Rubin <92750510+noam-vanta@users.noreply.github.com>
2022-02-21 23:28:14 +0000

    backing up zsh history

---
+++ backup-zsh-history.sh
@@ -0,0 +1,10 @@
+echo "backing up command history to dotfiles repo"
+if [ -d /workspaces/obisidian ]; then
+  cd /workspaces && git clone https://github.com/noam-vanta/dotfiles.git;
+fi
+
+cd /workspaces/dotfiles
+cp $HOME/.zhistory .
+git add .
+git commit -m "backing up zsh history"
+git push origin main
